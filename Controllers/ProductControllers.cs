bc4a278c87c7187033a88c64f8ac2716f93aeca2
root <root@vpsb3y32.s-host.host>
2022-04-26 15:31:32 +0200

    Додато Post/Put запити

--- Controllers/ProductControllers.cs
+++ Controllers/ProductControllers.cs
@@ -29,7 +29,49 @@ namespace First.Controllers
 	    return Ok(product);
 	}
 	
+	[HttpDelete("(id)")]
+	public IActionResult Delete (int id)
+	{
+	    products.Remove(products.SingleOrDefault(p =>p.id == id));
+	    return Ok();
+	}
 
+
+	private int NextProductId =>
+		products.Count()==0 ? 1 : products.Max (x => x.id) +1;
+
+	[HttpGet("AddProductId")]
+	public int GetNextProductId(){
+	    return NextProductId;
+	}
+//ADD TOVAR
+	[HttpPost]
+	public IActionResult Post(Product product)
+	{
+	    if (!ModelState.IsValid){
+		return BadRequest(ModelState);//Якщо відсутній обовязковий атрибут в моделі повертаємо БЕД
+	    }
+	    product.id = NextProductId;
+	    products.Add(product);
+	    return CreatedAtAction(nameof(Get), new {id = product.id},product);
+	}
+	[HttpPost("AddProduct")]
+	public IActionResult PostBody([FromBody] Product product) => Post(product);
+//EDIT TOVAR
+	[HttpPut]
+	public IActionResult Put(Product product)
+	{
+	if (!ModelState.IsValid){
+	return BadRequest(ModelState);
+        }
+        var storedProduct = products.SingleOrDefault (p => p.id ==product.id);
+        if (storedProduct == null) return NotFound();
+        storedProduct.Name = product.Name;
+        storedProduct.Price = product.Price;
+        return Ok(storedProduct);
+        }
+	[HttpPut("UpdateProduct")]
+	public IActionResult PutBody([FromBody] Product product) => Put(product);
 
 //	public IActionResult Index()
 //	{
